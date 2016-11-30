44db3c3d899d2d0ecfdde0827e4d72d1e247b056
Ian Dundas <contact@iandundas.co.uk>
2016-11-30 11:40:35 +0700

    Initial Commit

---
+++ HotTakeCore/HotTakeCore.h
@@ -0,0 +1,19 @@
+//
+//  HotTakeCore.h
+//  HotTakeCore
+//
+//  Created by Ian Dundas on 30/11/2016.
+//  Copyright © 2016 Ian Dundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HotTakeCore.
+FOUNDATION_EXPORT double HotTakeCoreVersionNumber;
+
+//! Project version string for HotTakeCore.
+FOUNDATION_EXPORT const unsigned char HotTakeCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HotTakeCore/PublicHeader.h>
+
+
